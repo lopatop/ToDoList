0e071aacd1ecdf26d2dd3e9981214129ddb80bed
lopatop <pavellopato327@gmail.com>
2025-01-06 10:08:30 +0100

    app

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,21 @@ import {Todolist} from "./components /Todolist.tsx";
 
 function App() {
 
- const tasks =[
-    {id: 1, title: "CSS", isDone: true},
-    {id: 2, title: "JS", isDone: true},
-    {id: 3, title: "React", isDone: true},
-    {id: 4, title: "Redux", isDone: false},
-  ]
+    const tasks = [
+        {id: 1, title: "CSS", isDone: true},
+        {id: 2, title: "JS", isDone: true},
+        {id: 3, title: "React", isDone: true},
+        {id: 4, title: "Redux", isDone: false},
+    ]
 
 
+    return (
+        <div className="app">
+            <Todolist title="What to learn"
+                      tasks={tasks}/>
 
-  return (
-      <div className="app">
-      <Todolist title="What to learn"
-                tasks={tasks}/>
-      </div>
-  )
+        </div>
+    )
 }
 
 export default App
