81c2a22d8396bc82743d7e83e3cdfd6c407c00aa
lopatop <pavellopato327@gmail.com>
2025-01-06 08:15:47 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css'
+import {Todolist} from "./components /Todolist.tsx";
+
+function App() {
+
+ const tasks =[
+    {id: 1, title: "CSS", isDone: true},
+    {id: 2, title: "JS", isDone: true},
+    {id: 3, title: "React", isDone: true},
+    {id: 4, title: "Redux", isDone: false},
+  ]
+
+
+
+  return (
+      <div className="app">
+      <Todolist title="What to learn"
+                tasks={tasks}/>
+      </div>
+  )
+}
+
+export default App
