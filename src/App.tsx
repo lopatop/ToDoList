e592789be4e7882b89adf33b3c640cba4bf187b7
lopatop <pavellopato327@gmail.com>
2025-01-07 23:05:00 +0100

    transferring functions from a to b

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,45 @@
 import './App.css'
-import {Todolist} from "./components /Todolist.tsx";
+import {onClickFilterHandlerType, Todolist} from "./components /Todolist.tsx";
+import {useState} from "react";
 
 function App() {
 
-    const tasks = [
+
+
+
+    const [tasks, setTask] = useState([
         {id: 1, title: "CSS", isDone: true},
         {id: 2, title: "JS", isDone: true},
         {id: 3, title: "React", isDone: true},
         {id: 4, title: "Redux", isDone: false},
-    ]
+    ])
+    const deleteTask = (taskId: number) => {
+        const updatedTasks = tasks.filter(task => task.id !== taskId)
+        setTask(updatedTasks)
+    }
+
+    const [filter, setFilter] = useState<onClickFilterHandlerType>('all')
+    const onClickFilterHandler = (title: onClickFilterHandlerType) => {
+        setFilter(title)
+    }
+
+    const currentTasksFoo = () => {
+        let currentTasks = tasks
+        if (filter === 'active') currentTasks = tasks.filter(t => !t.isDone)
+        if (filter === 'completed') currentTasks = tasks.filter(t => t.isDone)
+        return currentTasks
+    }
+
+    let tasksFilter = currentTasksFoo()
 
 
     return (
         <div className="app">
             <Todolist title="What to learn"
-                      tasks={tasks}/>
+                      tasks={tasksFilter}
+                      deleteTask={deleteTask}
+                      onClickFilterHandler={onClickFilterHandler}/>
+
 
         </div>
     )
