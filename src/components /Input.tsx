ea6c4162af608fbc3eaea10b34d9558901ec5fa2
lopatop <pavellopato327@gmail.com>
2025-01-12 23:13:08 +0100

    add function onKeyPressInputHandler

--- src/components /Input.tsx
+++ src/components /Input.tsx
@@ -1,21 +1,27 @@
-import {ChangeEvent} from "react";
+import {ChangeEvent,KeyboardEvent} from "react";
 
 type InputPropsType = {
     addTitle: string
     setAddTitle: (title: string) => void
+    addTasksFoo:()=>void
 
 }
 
 
 export const Input = (props: InputPropsType) => {
-    const {setAddTitle,addTitle} = props
+    const {setAddTitle,addTitle,addTasksFoo} = props
 
 
     const onChangeInputHandler = (e: ChangeEvent<HTMLInputElement>) => {
         setAddTitle(e.currentTarget.value)
     }
+    const onKeyPressInputHandler =(e:KeyboardEvent<HTMLInputElement>)=>{
+        if(e.key === 'Enter'){
+            addTasksFoo()
+        }
+    }
 
     return (
-        <input value={addTitle} onChange={onChangeInputHandler}/>
+        <input value={addTitle} onKeyPress={onKeyPressInputHandler} onChange={onChangeInputHandler}/>
     )
 }
