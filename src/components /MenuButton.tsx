052b71a42d1b3111a9948e54be9258ce38b48427
lopatop <pavellopato327@gmail.com>
2025-02-21 23:17:00 +0100

    MenuButton Material UI

---
+++ src/components /MenuButton.tsx
@@ -0,0 +1,24 @@
+import {Button, styled} from "@mui/material";
+
+type Props={
+    background?:string
+}
+
+export const MenuButton = styled(Button)<Props>(({background, theme}) => ({
+    minWidth: '110px',
+    fontWeight: 'bold',
+    borderRadius: '5px',
+    textTransform: 'capitalize',
+    margin: '0 10px',
+    padding: '8px 24px',
+    color: '#ffffff',
+    background: background || theme.palette.primary.light,
+    transition: "transform 0.5s ease, box-shadow 0.2s ease",
+
+    "&:hover":{
+        background: '#035363',
+        transform: 'translateY(-2px)',
+        opacity: '1',
+
+    }
+}))
