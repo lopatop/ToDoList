6be0446e22cc890b00389514c8ce53dc115ab278
lopatop <pavellopato327@gmail.com>
2025-02-28 21:40:41 +0100

    tasks selectors

---
+++ src/todolist.styles.ts
@@ -0,0 +1,5 @@
+import {SxProps} from "@mui/material";
+
+export const listItemStyle:SxProps = {p: '0', justifyContent: 'space-between',}
+export const filterButtonContainerStyle:SxProps = {display: 'flex', justifyContent: 'space-between', gap:'10px'}
+export const paperStyle:SxProps = {p: '30px', borderRadius: '15px'}
