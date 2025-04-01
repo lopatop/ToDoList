af5ff5f872e2b110e43a97b81e00977edbe2cf17
lopatop <pavellopato327@gmail.com>
2025-04-01 23:12:34 +0200

    store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,18 +1,17 @@
-import {combineReducers, configureStore} from '@reduxjs/toolkit'
-import {tasksReducer} from "../model/tasks-reducer.ts";
-import {todolistsReducer} from "../model/todolists-reducer.ts";
+import { combineReducers, configureStore } from "@reduxjs/toolkit"
+import { tasksReducer } from "@/features/todolists/model/tasks-reducer.ts"
+import { todolistsSlice } from "@/features/todolists/model/todolists-slice.ts"
+import { appReducer } from "./app-slice.ts"
 
 const rootReducer = combineReducers({
-    tasks: tasksReducer,
-    todolists: todolistsReducer,
+  tasks: tasksReducer,
+  todolists: todolistsSlice,
+  theme: appReducer,
 })
-
 
 export const store = configureStore({
-    reducer: rootReducer,
+  reducer: rootReducer,
 })
 
-
 export type RootState = ReturnType<typeof store.getState>
-
 export type AppDispatch = typeof store.dispatch
