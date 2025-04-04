738034cdbc234f8f8670ba4cbac48e1398e44c98
lopatop <pavellopato327@gmail.com>
2025-04-04 23:59:55 +0200

    tasks-selectors

--- src/features/todolists/model/tasks-selectors.ts
+++ src/features/todolists/model/tasks-selectors.ts
@@ -1,4 +1,4 @@
-import { TasksPropsType } from "@/features/todolists/model/tasks-reducer.ts"
+import { TasksPropsType } from "@/features/todolists/model/tasks-slice.ts"
 import { RootState } from "@/app/store.ts"
 
 export const selectTasks = (state: RootState): TasksPropsType => state.tasks
