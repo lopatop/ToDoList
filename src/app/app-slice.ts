8017a99a6106b1a97fe4021f9cb99f8c98fd7495
lopatop <pavellopato327@gmail.com>
2025-04-08 00:04:40 +0200

    app-slice

--- src/app/app-slice.ts
+++ src/app/app-slice.ts
@@ -1,3 +1,5 @@
+import { RequestStatus } from "@/common/types"
+
 export type ThemeMode = "dark" | "light"
 
 import { createSlice } from "@reduxjs/toolkit"
@@ -6,17 +8,22 @@ export const appSlice = createSlice({
   name: "app",
   initialState: {
     themeMode: "light" as ThemeMode,
+    status: "idle" as RequestStatus,
   },
   reducers: (create) => ({
     changeThemeModeAC: create.reducer<{ themeMode: ThemeMode }>((state, action) => {
       state.themeMode = action.payload.themeMode
     }),
+    setAppStatusAC: create.reducer<{ status: RequestStatus }>((state, action) => {
+      state.status = action.payload.status
+    }),
   }),
   selectors: {
     selectThemeMode: (state) => state.themeMode,
+    selectStatus: (state) => state.status,
   },
 })
 
-export const { changeThemeModeAC } = appSlice.actions
+export const { setAppStatusAC, changeThemeModeAC } = appSlice.actions
 export const appReducer = appSlice.reducer
-export const { selectThemeMode } = appSlice.selectors
+export const { selectStatus, selectThemeMode } = appSlice.selectors
