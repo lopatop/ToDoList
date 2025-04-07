4a7ed29b36c95792f2c3f28e33d5e06d8d5c7653
lopatop <pavellopato327@gmail.com>
2025-04-08 00:05:29 +0200

    header LinearProgress

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -1,53 +1,41 @@
+import AppBar from "@mui/material/AppBar"
+import Box from "@mui/material/Box"
+import Toolbar from "@mui/material/Toolbar"
+import Typography from "@mui/material/Typography"
+import IconButton from "@mui/material/IconButton"
+import MenuIcon from "@mui/icons-material/Menu"
+import { MenuButton } from "@/common/components/MenuButton/MenuButton.tsx"
+import { LinearProgress, Switch } from "@mui/material"
+import { changeThemeModeAC, selectStatus, selectThemeMode } from "@/app/app-slice.ts"
+import { getTheme } from "@/common/theme/theme.ts"
+import { useAppDispatch } from "@/common/hooks/useAppDispatch.ts"
+import { useAppSelector } from "@/common/hooks/useAppSelector.ts"
 
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import {MenuButton} from "@/components /MenuButton.tsx";
-import {Switch} from "@mui/material";
-import {changeThemeModeAC} from "@/app/app-reducer.ts";
-import {getTheme} from "@/common/theme/theme.ts";
-import {useAppDispatch} from "@/common/hooks/useAppDispatch.ts";
-import {selectThemeMode} from "@/app/app-selectors.ts";
-import {useAppSelector} from "@/common/hooks/useAppSelector.ts";
-
-
-export  function Header() {
-
-    const themeMode = useAppSelector(selectThemeMode)
-    const dispatch = useAppDispatch()
-    const theme = getTheme(themeMode)
-    const changeModeHandler = () => {
-        dispatch(changeThemeModeAC(themeMode === 'light' ? 'dark' : 'light'))
-    }
-
-    return (
-        <Box sx={{ flexGrow: 1, pb:'30px' }}>
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{ mr: 2 }}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        News
-                    </Typography>
-                    <MenuButton background={theme.palette.primary.dark} >Login</MenuButton>
-                    <MenuButton background={theme.palette.primary.dark}>LogOut</MenuButton>
-                    <MenuButton background={theme.palette.primary.dark}>FAQ</MenuButton>
-                    <Switch
-                        color={'default'}
-                        onChange={changeModeHandler}
-                    />
-                </Toolbar>
-            </AppBar>
-        </Box>
-    );
+export function Header() {
+  const themeMode = useAppSelector(selectThemeMode)
+  const dispatch = useAppDispatch()
+  const theme = getTheme(themeMode)
+  const changeModeHandler = () => {
+    dispatch(changeThemeModeAC({ themeMode: themeMode === "light" ? "dark" : "light" }))
+  }
+  const status = useAppSelector(selectStatus)
+  return (
+    <Box sx={{ flexGrow: 1, pb: "30px" }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <MenuButton background={theme.palette.primary.dark}>Login</MenuButton>
+          <MenuButton background={theme.palette.primary.dark}>LogOut</MenuButton>
+          <MenuButton background={theme.palette.primary.dark}>FAQ</MenuButton>
+          <Switch color={"default"} onChange={changeModeHandler} />
+        </Toolbar>
+        {status === "loading" && <LinearProgress />}
+      </AppBar>
+    </Box>
+  )
 }
