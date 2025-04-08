5b2ad4a3962a3a4b41f0bc62e019aab5b6e44c9f
lopatop <pavellopato327@gmail.com>
2025-04-08 20:26:46 +0200

    Tasks fetchTasksTC

--- src/features/todolists/ui/Todolists/TodolistItem/Tasks/Tasks.tsx
+++ src/features/todolists/ui/Todolists/TodolistItem/Tasks/Tasks.tsx
@@ -1,9 +1,14 @@
 import List from "@mui/material/List"
-import { onClickFilterHandlerType, TaskType } from "@/features/todolists/ui/Todolists/TodolistItem/TodolistItem.tsx"
+import { onClickFilterHandlerType } from "@/features/todolists/ui/Todolists/TodolistItem/TodolistItem.tsx"
 import { useAutoAnimate } from "@formkit/auto-animate/react"
 import { useAppSelector } from "@/common/hooks/useAppSelector.ts"
 import { selectTasks } from "@/features/todolists/model/tasks-selectors.ts"
 import { TaskItem } from "@/features/todolists/ui/Todolists/TodolistItem/Tasks/TaskItem/TaskItem.tsx"
+import { DomainTask } from "@/features/todolists/api/tasksApi.types.ts"
+import { TaskStatus } from "@/common/enums/enums.ts"
+import { useAppDispatch } from "@/common/hooks"
+import { useEffect } from "react"
+import { fetchTasksTC } from "@/features/todolists/model/tasks-slice.ts"
 
 type TodolistPropsType = {
   todolistId: string
@@ -17,15 +22,20 @@ export const Tasks = (props: TodolistPropsType) => {
   const [parent] = useAutoAnimate<HTMLUListElement>()
   const tasks = useAppSelector(selectTasks)
 
-  let currentTasks: TaskType[] = tasks[todolistId] || []
-  if (filter === "active") currentTasks = currentTasks.filter((t) => !t.isDone)
-  if (filter === "completed") currentTasks = currentTasks.filter((t) => t.isDone)
+  const dispatch = useAppDispatch()
+  useEffect(() => {
+    dispatch(fetchTasksTC(todolistId))
+  }, [])
+
+  let currentTasks: DomainTask[] = tasks[todolistId] || []
+  if (filter === "active") currentTasks = currentTasks.filter((t) => t.status === TaskStatus.New)
+  if (filter === "completed") currentTasks = currentTasks.filter((t) => t.status === TaskStatus.Completed)
 
   return (
     <>
       {currentTasks.length ? (
         <List ref={parent}>
-          {currentTasks.map((t: TaskType) => (
+          {currentTasks.map((t: DomainTask) => (
             <TaskItem key={t.id} todolistId={todolistId} t={t} />
           ))}
         </List>
