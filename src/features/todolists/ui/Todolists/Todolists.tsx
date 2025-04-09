d724d356ca5136ebebd41cb3d01e1604b9f55ddc
lopatop <pavellopato327@gmail.com>
2025-04-09 22:37:10 +0200

    Todolists fetchTodolistsTC

--- src/features/todolists/ui/Todolists/Todolists.tsx
+++ src/features/todolists/ui/Todolists/Todolists.tsx
@@ -1,16 +1,24 @@
 import Grid from "@mui/material/Grid"
 import Paper from "@mui/material/Paper"
-import { useAppSelector } from "@/common/hooks/useAppSelector.ts"
-import { selectTodolists } from "@/features/todolists/model/todolist-selectors.ts"
-import { TodolistItem } from "@/features/todolists/ui/Todolists/TodolistItem/TodolistItem.tsx"
-import { paperStyle } from "@/features/todolists/ui/Todolists/Todolists.styles.ts"
+import { useAppSelector } from "@/common/hooks/useAppSelector"
+import { selectTodolists } from "@/features/todolists/model/todolist-selectors"
+import { TodolistItem } from "@/features/todolists/ui/Todolists/TodolistItem/TodolistItem"
+import { paperStyle } from "@/features/todolists/ui/Todolists/Todolists.styles"
+import { useEffect } from "react"
+import { useAppDispatch } from "@/common/hooks"
+import { fetchTodolistsTC } from "@/features/todolists/model/todolists-slice"
 
 export const Todolists = () => {
   const todolist = useAppSelector(selectTodolists)
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(fetchTodolistsTC())
+  }, [])
 
   return (
     <Grid container>
-      {todolist.map((el) => {
+      {todolist?.map((el) => {
         return (
           <Grid item sx={{ p: "30px" }} key={el.id}>
             <Paper elevation={5} sx={paperStyle}>
