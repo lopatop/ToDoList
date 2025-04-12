3d1ba1f062cc19af6040794ebbcc8b1c4b770fe0
lopatop <pavellopato327@gmail.com>
2025-04-12 10:22:46 +0200

    app-slice

--- src/app/app-slice.ts
+++ src/app/app-slice.ts
@@ -9,6 +9,12 @@ export const appSlice = createSlice({
   initialState: {
     themeMode: "light" as ThemeMode,
     status: "idle" as RequestStatus,
+    error: null as string | null,
+  },
+  selectors: {
+    selectThemeMode: (state) => state.themeMode,
+    selectStatus: (state) => state.status,
+    selectError: (state) => state.error,
   },
   reducers: (create) => ({
     changeThemeModeAC: create.reducer<{ themeMode: ThemeMode }>((state, action) => {
@@ -17,13 +23,12 @@ export const appSlice = createSlice({
     setAppStatusAC: create.reducer<{ status: RequestStatus }>((state, action) => {
       state.status = action.payload.status
     }),
+    setAppErrorAC: create.reducer<{ error: string | null }>((state, action) => {
+      state.error = action.payload.error
+    }),
   }),
-  selectors: {
-    selectThemeMode: (state) => state.themeMode,
-    selectStatus: (state) => state.status,
-  },
 })
 
-export const { setAppStatusAC, changeThemeModeAC } = appSlice.actions
+export const { setAppStatusAC, changeThemeModeAC, setAppErrorAC } = appSlice.actions
 export const appReducer = appSlice.reducer
-export const { selectStatus, selectThemeMode } = appSlice.selectors
+export const { selectStatus, selectThemeMode, selectError } = appSlice.selectors
