f55bcbcb629d03b7ad982f37c62c8dd0aa245cbc
lopatop <pavellopato327@gmail.com>
2025-04-21 01:14:25 +0200

    handleServerNetworkError add ZodError

--- src/common/utils/handleServerNetworkError.ts
+++ src/common/utils/handleServerNetworkError.ts
@@ -1,6 +1,7 @@
 import { setAppErrorAC, setAppStatusAC } from "@/app/app-slice.ts"
 import { Dispatch } from "@reduxjs/toolkit"
 import axios, { AxiosError } from "axios"
+import { ZodError } from "zod"
 
 export const handleServerNetworkError = (error: unknown, dispatch: Dispatch) => {
   let errorMessage
@@ -8,6 +9,9 @@ export const handleServerNetworkError = (error: unknown, dispatch: Dispatch) => 
     errorMessage = error.response?.data?.message || error.message
   } else if (error instanceof AxiosError) {
     errorMessage = error.message
+  } else if (error instanceof ZodError) {
+    console.table(error.issues)
+    errorMessage = "Zod error. Смотри консоль"
   } else {
     errorMessage = JSON.stringify(error)
   }
