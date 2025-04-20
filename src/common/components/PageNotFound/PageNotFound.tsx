87a93adc9288d6535ae4e375e6c03cb46814b1c0
lopatop <pavellopato327@gmail.com>
2025-04-21 01:14:00 +0200

    PageNotFound add Button

--- src/common/components/PageNotFound/PageNotFound.tsx
+++ src/common/components/PageNotFound/PageNotFound.tsx
@@ -1,8 +1,15 @@
 import styles from "./PageNotFound.module.css"
+import Button from "@mui/material/Button"
+import { Link } from "react-router"
 
 export const PageNotFound = () => (
   <>
     <h1 className={styles.title}>404</h1>
     <h2 className={styles.subtitle}>page not found</h2>
+    <div className={styles.buttonBlock}>
+      <Button variant={"contained"} component={Link} to="/" sx={{ textAlign: "center", width: "350px" }}>
+        home page
+      </Button>
+    </div>
   </>
 )
