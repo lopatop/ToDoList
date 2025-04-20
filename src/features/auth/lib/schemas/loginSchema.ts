f3188231d8c4cbfe1152e3235f718cff7710d1ef
lopatop <pavellopato327@gmail.com>
2025-04-21 01:13:35 +0200

    loginSchema

--- src/features/auth/lib/schemas/loginSchema.ts
+++ src/features/auth/lib/schemas/loginSchema.ts
@@ -2,7 +2,7 @@ import { z } from "zod"
 
 export const loginSchema = z.object({
   email: z.string().min(3, "Email is required").email("Incorrect email address"),
-  password: z.string().min(6, "Password must be at least 6 characters"),
+  password: z.string({ message: "Password is required" }).min(3, "Password must be at least 3 characters long"),
   rememberMe: z.boolean(),
 })
 
