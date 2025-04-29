2c226f37aa39ed36474017ecf62a2eb588e06059
lopatop <pavellopato327@gmail.com>
2025-04-29 20:43:31 +0200

    interceptors

--- src/common/instance/instance.ts
+++ src/common/instance/instance.ts
@@ -1,9 +1,16 @@
 import axios from "axios"
+import {AUTH_TOKEN} from "@/common/constant";
 
 export const instance = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
   headers: {
-    Authorization: `Bearer ${import.meta.env.VITE_AUTH_TOKEN}`,
     "Api-Key": import.meta.env.VITE_API_KEY,
   },
 })
+
+instance.interceptors.request.use( ((config) =>{
+  config.headers['Authorization'] = `Bearer ${localStorage.getItem(AUTH_TOKEN)}`
+  return config;
+}))
+
+      
