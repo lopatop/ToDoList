835ad7fe00566ca8aa5daae45f68818fcd8bd21f
lopatop <pavellopato327@gmail.com>
2025-04-29 20:41:23 +0200

    refactor component App (ThemeProvider)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,57 @@
-import "./App.css"
+import s from"./App.module.css"
 import { Header } from "@/common/components"
 import CssBaseline from "@mui/material/CssBaseline"
-import { ThemeProvider } from "@mui/material"
+import {CircularProgress, ThemeProvider} from "@mui/material"
 import { useSelector } from "react-redux"
 import { getTheme } from "@/common/theme"
-import { Main } from "@/app/Main"
 import { selectThemeMode } from "@/app/app-slice"
 import { ErrorSnackbar } from "@/common/components/ErrorSnackbar/ErrorSnackbar.tsx"
+import { Routing } from "@/common/routing/Routing.tsx"
+import {useAppDispatch, useAppSelector} from "@/common/hooks";
+import {useEffect, useState} from "react";
+import {meTC} from "@/features/auth/model/auth-slice.ts";
+import {selectTasks, selectTodolists} from "@/features/todolists/model";
 
 function App() {
+  const [isInitialized, setIsInitialized] = useState(false)
+
+
   const themeMode = useSelector(selectThemeMode)
   const theme = getTheme(themeMode)
+  const dispatch = useAppDispatch()
+
+    const tasks = useAppSelector(selectTasks)
+    const todolists = useAppSelector(selectTodolists)
+
+    useEffect(() => {
+        console.log(' таски: ', tasks)
+        console.log(' тудушки: ', todolists)
+    }, [tasks,todolists]);
+
+
+  useEffect(() => {
+    dispatch(meTC()).finally(()=>{setIsInitialized(true)})
+  }, []);
+
+  if (!isInitialized) {
+    return (
+        <ThemeProvider theme={theme}>
+        <div className={s.circularProgressContainer}>
+          <CircularProgress size={150} thickness={3} />
+        </div>
+        </ThemeProvider>
+    )
+  }
+
+
+
 
   return (
     <ThemeProvider theme={theme}>
       <div className="app">
         <CssBaseline />
         <Header />
-        <Main />
+        <Routing />
         <ErrorSnackbar />
       </div>
     </ThemeProvider>
