7af8fe1607309027c55aba929967336cb2ed95dc
lopatop <pavellopato327@gmail.com>
2025-04-29 20:42:47 +0200

    Button onClickHandler

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -10,6 +10,9 @@ import { changeThemeModeAC, selectStatus, selectThemeMode } from "@/app/app-slic
 import { getTheme } from "@/common/theme/theme"
 import { useAppDispatch } from "@/common/hooks/useAppDispatch"
 import { useAppSelector } from "@/common/hooks/useAppSelector"
+import {logoutTC, selectIsLoggedIn} from "@/features/auth/model/auth-slice.ts";
+import {useNavigate} from "react-router";
+import {Path} from "@/common/routing/Routing.tsx";
 
 export function Header() {
   const themeMode = useAppSelector(selectThemeMode)
@@ -18,20 +21,28 @@ export function Header() {
   const changeModeHandler = () => {
     dispatch(changeThemeModeAC({ themeMode: themeMode === "light" ? "dark" : "light" }))
   }
+  const isLoggedIn = useAppSelector(selectIsLoggedIn)
   const status = useAppSelector(selectStatus)
+  const navigate = useNavigate();
+  
+
+  const onClickLogOutHandler = ()=>{
+    dispatch(logoutTC())
+  }
+
+  const onClickFaqHandler =()=>{
+    navigate(Path.Faq)
+  }
+
+
   return (
     <Box sx={{ flexGrow: 1, pb: "30px" }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <MenuButton background={theme.palette.primary.dark}>Login</MenuButton>
-          <MenuButton background={theme.palette.primary.dark}>LogOut</MenuButton>
-          <MenuButton background={theme.palette.primary.dark}>FAQ</MenuButton>
+          <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}><MenuIcon /></IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>News</Typography>
+          {isLoggedIn && <MenuButton background={theme.palette.primary.dark} onClick={onClickLogOutHandler}>LogOut</MenuButton> }
+          {isLoggedIn &&  <MenuButton background={theme.palette.primary.dark} onClick={onClickFaqHandler} >FAQ</MenuButton> }
           <Switch color={"default"} onChange={changeModeHandler} />
         </Toolbar>
         {status === "loading" && <LinearProgress />}
