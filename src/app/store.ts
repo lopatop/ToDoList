67acbf2edef06f50938ebd5a4449515393da9b47
lopatop <pavellopato327@gmail.com>
2025-05-02 23:53:35 +0200

    add authReducer

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,11 +2,13 @@ import { combineReducers, configureStore } from "@reduxjs/toolkit"
 import { tasksReducer, tasksSlice } from "@/features/todolists/model/tasks-slice"
 import { todolistsReducer, todolistsSlice } from "@/features/todolists/model/todolists-slice"
 import { appReducer, appSlice } from "./app-slice"
+import {authReducer, authSlice} from "@/features/auth/model/auth-slice.ts";
 
 const rootReducer = combineReducers({
   [tasksSlice.name]: tasksReducer,
   [todolistsSlice.name]: todolistsReducer,
   [appSlice.name]: appReducer,
+  [authSlice.name]: authReducer,
 })
 
 export const store = configureStore({
