f3253e1c0fa329354d0c5b00ab0d20ba603f0f0e
lopatop <pavellopato327@gmail.com>
2025-05-02 23:54:23 +0200

    refactor

--- src/app/Main.tsx
+++ src/app/Main.tsx
@@ -1,20 +1,20 @@
 import Grid from "@mui/material/Grid"
-import { CreateItemForm } from "@/common/components/CreateItemForm/CreateItemForm"
-import { createTodolistTC } from "@/features/todolists/model/todolists-slice"
-import { useAppDispatch } from "@/common/hooks/useAppDispatch"
-import { Todolists } from "@/features/todolists/ui/Todolists/Todolists"
+import {CreateItemForm} from "@/common/components/CreateItemForm/CreateItemForm"
+import {createTodolistTC} from "@/features/todolists/model/todolists-slice"
+import {useAppDispatch} from "@/common/hooks/useAppDispatch"
+import {Todolists} from "@/features/todolists/ui/Todolists/Todolists"
 
 export const Main = () => {
-  const dispatch = useAppDispatch()
-  const addTodolist = (titleTodolist: string) => {
-    dispatch(createTodolistTC({ title: titleTodolist }))
-  }
-  return (
-    <>
-      <Grid container sx={{ ml: "30px" }}>
-        <CreateItemForm addItem={addTodolist} />
-      </Grid>
-      <Todolists />
-    </>
-  )
+    const dispatch = useAppDispatch()
+    const addTodolist = (titleTodolist: string) => {
+        dispatch(createTodolistTC({title: titleTodolist}))
+    }
+    return (
+        <>
+            <Grid container sx={{ml: "30px"}}>
+                <CreateItemForm addItem={addTodolist}/>
+            </Grid>
+            <Todolists/>
+        </>
+    )
 }
