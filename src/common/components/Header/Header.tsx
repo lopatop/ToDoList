a9c07a5a02c1028c624c6256056f6d99bb14c2ed
lopatop <pavellopato327@gmail.com>
2025-05-08 23:28:56 +0200

    useLogoutMutation()

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -4,49 +4,63 @@ import Toolbar from "@mui/material/Toolbar"
 import Typography from "@mui/material/Typography"
 import IconButton from "@mui/material/IconButton"
 import MenuIcon from "@mui/icons-material/Menu"
-import { MenuButton } from "@/common/components/MenuButton/MenuButton"
-import { LinearProgress, Switch } from "@mui/material"
-import { changeThemeModeAC, selectStatus, selectThemeMode } from "@/app/app-slice"
-import { getTheme } from "@/common/theme/theme"
-import { useAppDispatch } from "@/common/hooks/useAppDispatch"
-import { useAppSelector } from "@/common/hooks/useAppSelector"
-import {logoutTC, selectIsLoggedIn} from "@/features/auth/model/auth-slice.ts";
+import {MenuButton} from "@/common/components/MenuButton/MenuButton"
+import {LinearProgress, Switch} from "@mui/material"
+import {changeThemeModeAC, selectIsLoggedIn, selectStatus, selectThemeMode, setIsLoggedInAC} from "@/app/app-slice"
+import {getTheme} from "@/common/theme/theme"
+import {useAppDispatch} from "@/common/hooks/useAppDispatch"
+import {useAppSelector} from "@/common/hooks/useAppSelector"
 import {useNavigate} from "react-router";
 import {Path} from "@/common/routing/Routing.tsx";
+import {AUTH_TOKEN} from "@/common/constant";
+import {useLogoutMutation} from "@/features/auth/api/authApi.ts";
+import {ResultCode} from "@/common/enums";
+import {clearDataAC} from "@/common/actions";
 
 export function Header() {
-  const themeMode = useAppSelector(selectThemeMode)
-  const dispatch = useAppDispatch()
-  const theme = getTheme(themeMode)
-  const changeModeHandler = () => {
-    dispatch(changeThemeModeAC({ themeMode: themeMode === "light" ? "dark" : "light" }))
-  }
-  const isLoggedIn = useAppSelector(selectIsLoggedIn)
-  const status = useAppSelector(selectStatus)
-  const navigate = useNavigate();
-  
-
-  const onClickLogOutHandler = ()=>{
-    dispatch(logoutTC())
-  }
-
-  const onClickFaqHandler =()=>{
-    navigate(Path.Faq)
-  }
+    const themeMode = useAppSelector(selectThemeMode)
+    const dispatch = useAppDispatch()
+    const theme = getTheme(themeMode)
+    const changeModeHandler = () => {
+        dispatch(changeThemeModeAC({themeMode: themeMode === "light" ? "dark" : "light"}))
+    }
+    const isLoggedIn = useAppSelector(selectIsLoggedIn)
+    const status = useAppSelector(selectStatus)
+    const navigate = useNavigate();
+    const [logout] = useLogoutMutation()
 
 
-  return (
-    <Box sx={{ flexGrow: 1, pb: "30px" }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}><MenuIcon /></IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>News</Typography>
-          {isLoggedIn && <MenuButton background={theme.palette.primary.dark} onClick={onClickLogOutHandler}>LogOut</MenuButton> }
-          {isLoggedIn &&  <MenuButton background={theme.palette.primary.dark} onClick={onClickFaqHandler} >FAQ</MenuButton> }
-          <Switch color={"default"} onChange={changeModeHandler} />
-        </Toolbar>
-        {status === "loading" && <LinearProgress />}
-      </AppBar>
-    </Box>
-  )
+    const onClickLogOutHandler = () => {
+        logout().then((res) => {
+            if (res.data?.resultCode === ResultCode.Success) {
+                dispatch(setIsLoggedInAC({isLoggedIn: false}))
+                localStorage.removeItem(AUTH_TOKEN)
+                dispatch(clearDataAC())
+            }
+        })
+    }
+
+
+    const onClickFaqHandler = () => {
+        navigate(Path.Faq)
+    }
+
+
+    return (
+        <Box sx={{flexGrow: 1, pb: "30px"}}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton size="large" edge="start" color="inherit" aria-label="menu"
+                                sx={{mr: 2}}><MenuIcon/></IconButton>
+                    <Typography variant="h6" component="div" sx={{flexGrow: 1}}>News</Typography>
+                    {isLoggedIn && <MenuButton background={theme.palette.primary.dark}
+                                               onClick={onClickLogOutHandler}>LogOut</MenuButton>}
+                    {isLoggedIn && <MenuButton background={theme.palette.primary.dark}
+                                               onClick={onClickFaqHandler}>FAQ</MenuButton>}
+                    <Switch color={"default"} onChange={changeModeHandler}/>
+                </Toolbar>
+                {status === "loading" && <LinearProgress/>}
+            </AppBar>
+        </Box>
+    )
 }
