95602b559a70514ed335a3dc7f821df90585917f
lopatop <pavellopato327@gmail.com>
2025-05-08 23:28:21 +0200

    useMeQuery()

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,61 +1,57 @@
-import s from"./App.module.css"
-import { Header } from "@/common/components"
+import s from "./App.module.css"
+import {Header} from "@/common/components"
 import CssBaseline from "@mui/material/CssBaseline"
 import {CircularProgress, ThemeProvider} from "@mui/material"
-import { useSelector } from "react-redux"
-import { getTheme } from "@/common/theme"
-import { selectThemeMode } from "@/app/app-slice"
-import { ErrorSnackbar } from "@/common/components/ErrorSnackbar/ErrorSnackbar.tsx"
-import { Routing } from "@/common/routing/Routing.tsx"
-import {useAppDispatch, useAppSelector} from "@/common/hooks";
+import {useSelector} from "react-redux"
+import {getTheme} from "@/common/theme"
+import {selectThemeMode, setIsLoggedInAC} from "@/app/app-slice"
+import {ErrorSnackbar} from "@/common/components/ErrorSnackbar/ErrorSnackbar.tsx"
+import {Routing} from "@/common/routing/Routing.tsx"
+import {useAppDispatch} from "@/common/hooks";
 import {useEffect, useState} from "react";
-import {meTC} from "@/features/auth/model/auth-slice.ts";
-import {selectTasks, selectTodolists} from "@/features/todolists/model";
+import {useMeQuery} from "@/features/auth/api/authApi.ts";
+import {ResultCode} from "@/common/enums";
 
 function App() {
-  const [isInitialized, setIsInitialized] = useState(false)
+    const [isInitialized, setIsInitialized] = useState(false)
+
+    const {data, isLoading} = useMeQuery()
 
 
-  const themeMode = useSelector(selectThemeMode)
-  const theme = getTheme(themeMode)
-  const dispatch = useAppDispatch()
+    const themeMode = useSelector(selectThemeMode)
+    const theme = getTheme(themeMode)
+    const dispatch = useAppDispatch()
 
-    const tasks = useAppSelector(selectTasks)
-    const todolists = useAppSelector(selectTodolists)
 
     useEffect(() => {
-        console.log(' таски: ', tasks)
-        console.log(' тудушки: ', todolists)
-    }, [tasks,todolists]);
+        if(isLoading) return
+        setIsInitialized(true)
+        if(data?.resultCode === ResultCode.Success){
+            dispatch(setIsLoggedInAC({isLoggedIn: true}))
+        }
+    }, [isLoading]);
+
+    if (!isInitialized) {
+        return (
+            <ThemeProvider theme={theme}>
+                <div className={s.circularProgressContainer}>
+                    <CircularProgress size={150} thickness={3}/>
+                </div>
+            </ThemeProvider>
+        )
+    }
 
 
-  useEffect(() => {
-    dispatch(meTC()).finally(()=>{setIsInitialized(true)})
-  }, []);
-
-  if (!isInitialized) {
     return (
         <ThemeProvider theme={theme}>
-        <div className={s.circularProgressContainer}>
-          <CircularProgress size={150} thickness={3} />
-        </div>
+            <div className="app">
+                <CssBaseline/>
+                <Header/>
+                <Routing/>
+                <ErrorSnackbar/>
+            </div>
         </ThemeProvider>
     )
-  }
-
-
-
-
-  return (
-    <ThemeProvider theme={theme}>
-      <div className="app">
-        <CssBaseline />
-        <Header />
-        <Routing />
-        <ErrorSnackbar />
-      </div>
-    </ThemeProvider>
-  )
 }
 
 export default App
