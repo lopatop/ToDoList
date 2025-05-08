6f8ff159e259dcce3134e380477f2841e4852a6d
lopatop <pavellopato327@gmail.com>
2025-05-08 23:28:42 +0200

    add setIsLoggedInAC

--- src/app/app-slice.ts
+++ src/app/app-slice.ts
@@ -10,11 +10,13 @@ export const appSlice = createSlice({
     themeMode: "light" as ThemeMode,
     status: "idle" as RequestStatus,
     error: null as string | null,
+    isLoggedIn: false,
   },
   selectors: {
     selectThemeMode: (state) => state.themeMode,
     selectStatus: (state) => state.status,
     selectError: (state) => state.error,
+    selectIsLoggedIn: (state) => state.isLoggedIn,
   },
   reducers: (create) => ({
     changeThemeModeAC: create.reducer<{ themeMode: ThemeMode }>((state, action) => {
@@ -26,9 +28,12 @@ export const appSlice = createSlice({
     setAppErrorAC: create.reducer<{ error: string | null }>((state, action) => {
       state.error = action.payload.error
     }),
+    setIsLoggedInAC: create.reducer<{ isLoggedIn: boolean }>((state, action)=>{
+      state.isLoggedIn = action.payload.isLoggedIn
+    })
   }),
 })
 
-export const { setAppStatusAC, changeThemeModeAC, setAppErrorAC } = appSlice.actions
+export const { setAppStatusAC, changeThemeModeAC, setAppErrorAC,setIsLoggedInAC } = appSlice.actions
 export const appReducer = appSlice.reducer
-export const { selectStatus, selectThemeMode, selectError } = appSlice.selectors
+export const { selectStatus, selectThemeMode, selectError,selectIsLoggedIn } = appSlice.selectors
