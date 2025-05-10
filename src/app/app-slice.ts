f85019abc70076c91a47fc45f8a2f62dd6d68488
lopatop <pavellopato327@gmail.com>
2025-05-10 23:40:38 +0200

    add addMatcher in extraReducers

--- src/app/app-slice.ts
+++ src/app/app-slice.ts
@@ -1,39 +1,56 @@
-import { RequestStatus } from "@/common/types"
+import {RequestStatus} from "@/common/types"
 
 export type ThemeMode = "dark" | "light"
 
-import { createSlice } from "@reduxjs/toolkit"
+import {createSlice, isFulfilled, isPending, isRejected} from "@reduxjs/toolkit"
+import {todolistsApi} from "@/features/todolists/api/todolistsApi.ts";
+import {tasksApi} from "@/features/todolists/api/tasksApi.ts";
 
 export const appSlice = createSlice({
-  name: "app",
-  initialState: {
-    themeMode: "light" as ThemeMode,
-    status: "idle" as RequestStatus,
-    error: null as string | null,
-    isLoggedIn: false,
-  },
-  selectors: {
-    selectThemeMode: (state) => state.themeMode,
-    selectStatus: (state) => state.status,
-    selectError: (state) => state.error,
-    selectIsLoggedIn: (state) => state.isLoggedIn,
-  },
-  reducers: (create) => ({
-    changeThemeModeAC: create.reducer<{ themeMode: ThemeMode }>((state, action) => {
-      state.themeMode = action.payload.themeMode
+    name: "app",
+    initialState: {
+        themeMode: "light" as ThemeMode,
+        status: "idle" as RequestStatus,
+        error: null as string | null,
+        isLoggedIn: false,
+    },
+    selectors: {
+        selectThemeMode: (state) => state.themeMode,
+        selectStatus: (state) => state.status,
+        selectError: (state) => state.error,
+        selectIsLoggedIn: (state) => state.isLoggedIn,
+    },
+    extraReducers: (builder) => {
+        builder.addMatcher(isPending, ((state, action) => {
+            if (todolistsApi.endpoints.getTodolists.matchPending(action) ||
+                tasksApi.endpoints.getTasks.matchPending(action)) {
+                return
+            }
+            state.status = 'loading'
+        }))
+            .addMatcher(isFulfilled, state => {
+                state.status = 'succeeded'
+            })
+            .addMatcher(isRejected, state => {
+                state.status = 'failed'
+            })
+    },
+    reducers: (create) => ({
+        changeThemeModeAC: create.reducer<{ themeMode: ThemeMode }>((state, action) => {
+            state.themeMode = action.payload.themeMode
+        }),
+        setAppStatusAC: create.reducer<{ status: RequestStatus }>((state, action) => {
+            state.status = action.payload.status
+        }),
+        setAppErrorAC: create.reducer<{ error: string | null }>((state, action) => {
+            state.error = action.payload.error
+        }),
+        setIsLoggedInAC: create.reducer<{ isLoggedIn: boolean }>((state, action) => {
+            state.isLoggedIn = action.payload.isLoggedIn
+        })
     }),
-    setAppStatusAC: create.reducer<{ status: RequestStatus }>((state, action) => {
-      state.status = action.payload.status
-    }),
-    setAppErrorAC: create.reducer<{ error: string | null }>((state, action) => {
-      state.error = action.payload.error
-    }),
-    setIsLoggedInAC: create.reducer<{ isLoggedIn: boolean }>((state, action)=>{
-      state.isLoggedIn = action.payload.isLoggedIn
-    })
-  }),
 })
 
-export const { setAppStatusAC, changeThemeModeAC, setAppErrorAC,setIsLoggedInAC } = appSlice.actions
+export const {setAppStatusAC, changeThemeModeAC, setAppErrorAC, setIsLoggedInAC} = appSlice.actions
 export const appReducer = appSlice.reducer
-export const { selectStatus, selectThemeMode, selectError,selectIsLoggedIn } = appSlice.selectors
+export const {selectStatus, selectThemeMode, selectError, selectIsLoggedIn} = appSlice.selectors
