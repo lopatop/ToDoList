a618f4832ed7a7046e534bdab67b1bb4b93c36f8
lopatop <pavellopato327@gmail.com>
2025-05-11 22:16:09 +0200

    add zod schemes FieldErrorSchema and defaultResponseSchema

--- src/common/types/types.ts
+++ src/common/types/types.ts
@@ -1,13 +1,25 @@
-export type FieldError = {
-  error: string
-  field: string
-}
+import {z} from "zod";
+import {ResultCode} from "@/common/enums";
 
-export type BaseResponse<T = {}> = {
-  data: T
-  messages: string[]
-  fieldsErrors?: FieldError[]
-  resultCode: number
-}
+
+export const FieldErrorSchema = z.object({
+    error: z.string(),
+    field: z.string()
+})
+
+
+export const baseResponseSchema = <T extends z.ZodTypeAny>(schema: T) =>
+    z.object({
+        data: schema,
+        messages: z.string().array(),
+        fieldsErrors: FieldErrorSchema.array(),
+        resultCode: z.nativeEnum(ResultCode),
+    })
+
+
+
+export const defaultResponseSchema = baseResponseSchema(z.object({}))
+
+export type DefaultResponse = z.infer<typeof defaultResponseSchema>
 
 export type RequestStatus = "idle" | "loading" | "succeeded" | "failed"
