c7a1335394ceb9b091de43d98b72311a6cfacd0c
lopatop <pavellopato327@gmail.com>
2025-05-11 22:12:03 +0200

    delete todolists slice and selectors, test

--- src/features/todolists/model/todolist-selectors.ts
+++
@@ -1,4 +0,0 @@
-import { DomainTodolist } from "@/features/todolists/model/todolists-slice"
-import { RootState } from "@/app/store"
-
-export const selectTodolists = (state: RootState): DomainTodolist[] => state.todolists
