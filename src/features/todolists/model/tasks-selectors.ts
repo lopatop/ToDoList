5603bcf9fa63ce9811996d8a02b80ca45699902b
lopatop <pavellopato327@gmail.com>
2025-05-11 22:11:42 +0200

    delete tasks slice and selectors, test

--- src/features/todolists/model/tasks-selectors.ts
+++
@@ -1,4 +0,0 @@
-import { TasksPropsType } from "@/features/todolists/model/tasks-slice"
-import { RootState } from "@/app/store"
-
-export const selectTasks = (state: RootState): TasksPropsType => state.tasks
