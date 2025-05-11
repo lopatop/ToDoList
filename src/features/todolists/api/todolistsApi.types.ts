21350ecccb4dea9e2414943a604780adef42409b
lopatop <pavellopato327@gmail.com>
2025-05-11 22:12:19 +0200

    add createTodolistResponseSchema

--- src/features/todolists/api/todolistsApi.types.ts
+++ src/features/todolists/api/todolistsApi.types.ts
@@ -1,10 +1,20 @@
 import { z } from "zod"
+import {baseResponseSchema} from "@/common/types/types.ts";
 
-export type Todolist = z.infer<typeof TodolistSchema>
 
-export const TodolistSchema = z.object({
+export type Todolist = z.infer<typeof todolistSchema>
+
+export const todolistSchema = z.object({
   addedDate: z.string().datetime({ local: true }),
   id: z.string(),
   order: z.number(),
   title: z.string(),
 })
+
+export const createTodolistResponseSchema = baseResponseSchema(
+    z.object({
+      item:todolistSchema
+    })
+)
+
+export type CreateTodolistResponse = z.infer<typeof createTodolistResponseSchema>
