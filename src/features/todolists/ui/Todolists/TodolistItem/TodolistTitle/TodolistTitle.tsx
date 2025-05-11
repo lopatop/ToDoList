8460a418d381d9e281443c9d82f4f459da5e7a68
lopatop <pavellopato327@gmail.com>
2025-05-11 22:14:08 +0200

    refactor deleteTodolist and changeTitleTodolist

--- src/features/todolists/ui/Todolists/TodolistItem/TodolistTitle/TodolistTitle.tsx
+++ src/features/todolists/ui/Todolists/TodolistItem/TodolistTitle/TodolistTitle.tsx
@@ -1,41 +1,67 @@
-import { EditableSpan } from "@/common/components/EditableSpan/EditableSpan"
 import IconButton from "@mui/material/IconButton"
 import Delete from "@mui/icons-material/Delete"
-import { changeTodolistTitleTC, deleteTodolistTC, DomainTodolist } from "@/features/todolists/model/todolists-slice"
-import { useAppDispatch } from "@/common/hooks/useAppDispatch"
+import {
+    todolistsApi,
+    useChangeTodolistTitleMutation,
+    useDeleteTodolistMutation
+} from "@/features/todolists/api/todolistsApi.ts";
+import {useAppDispatch} from "@/common/hooks";
+import {DomainTodolist} from "@/features/todolists/lib/types";
+import {EditableSpan} from "@/common/components";
+import {RequestStatus} from "@/common/types/types.ts";
 
 type TodolistPropsType = {
-  todolist: DomainTodolist
-  disabled: boolean
+    todolist: DomainTodolist
+    disabled: boolean
 }
 
+
+
+
 export const TodolistTitle = (props: TodolistPropsType) => {
-  const { todolist } = props
+    const {todolist} = props
 
-  const dispatch = useAppDispatch()
 
-  const onClickDeleteTodolistHandler = () => {
-    dispatch(deleteTodolistTC({ id: todolist.id }))
-  }
+    const [changeTodolistTitle, {}] = useChangeTodolistTitleMutation()
+    const [deleteTodolist] = useDeleteTodolistMutation()
+    const dispatch = useAppDispatch()
 
-  const removeTitleTodolistHandler = (newTitle: string) => {
-    dispatch(changeTodolistTitleTC({ id: todolist.id, title: newTitle }))
-  }
 
-  return (
-    <h3>
-      <EditableSpan
-        title={todolist.title}
-        changeTitleItem={(newTitleTask) => removeTitleTodolistHandler(newTitleTask)}
-        disabled={todolist.entityStatus === "loading"}
-      />
-      <IconButton
-        aria-label="delete"
-        onClick={onClickDeleteTodolistHandler}
-        disabled={todolist.entityStatus === "loading"}
-      >
-        <Delete />
-      </IconButton>
-    </h3>
-  )
+    const changeTodolistStatus = (entityStatus: RequestStatus) =>{
+        dispatch(todolistsApi.util.updateQueryData("getTodolists", undefined, (state) => {
+            const todo = state.find(t => t.id === todolist.id)
+            if (todo){
+                todo.entityStatus = entityStatus
+            }
+        }))
+    }
+
+
+    const deleteTodolistHandler = () => {
+        changeTodolistStatus("loading")
+        deleteTodolist(todolist.id).unwrap().catch(()=>{
+            changeTodolistStatus("idle")
+        })
+    }
+    
+ const changeTitleTodolistHandler = (newTitle: string) => {
+        changeTodolistTitle({id: todolist.id, title: newTitle})
+    }
+
+    return (
+        <h3>
+            <EditableSpan
+                title={todolist.title}
+                changeTitleItem={changeTitleTodolistHandler}
+                disabled={todolist.entityStatus === "loading"}
+            />
+            <IconButton
+                aria-label="delete"
+                onClick={deleteTodolistHandler}
+                disabled={todolist.entityStatus === "loading"}
+            >
+                <Delete/>
+            </IconButton>
+        </h3>
+    )
 }
