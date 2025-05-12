0d227e76ba2cac8e93e60439ef29ccc1ed5323ec
lopatop <pavellopato327@gmail.com>
2025-05-12 20:52:54 +0200

    useCreateTaskMutation

--- src/features/todolists/ui/Todolists/TodolistItem/TodolistItem.tsx
+++ src/features/todolists/ui/Todolists/TodolistItem/TodolistItem.tsx
@@ -1,13 +1,11 @@
 import { CreateItemForm } from "@/common/components"
-
-import { useAppDispatch } from "@/common/hooks/useAppDispatch"
 import { TodolistTitle } from "@/features/todolists/ui/Todolists/TodolistItem/TodolistTitle/TodolistTitle"
 import { Tasks } from "@/features/todolists/ui/Todolists/TodolistItem/Tasks/Tasks"
 import { FilterButtons } from "@/features/todolists/ui/Todolists/TodolistItem/FilterButtons/FilterButtons"
-import { createTaskTC } from "@/features/todolists/model/tasks-slice"
-import { DomainTodolist } from "@/features/todolists/model/todolists-slice.ts"
+import {useCreateTaskMutation} from "@/features/todolists/api/tasksApi.ts";
+import {DomainTodolist} from "@/features/todolists/lib/types";
 
-export type onClickFilterHandlerType = "all" | "active" | "completed"
+
 
 type TodolistPropsType = {
   todolist: DomainTodolist
@@ -15,16 +13,19 @@ type TodolistPropsType = {
 
 export const TodolistItem = (props: TodolistPropsType) => {
   const { todolist } = props
-  const dispatch = useAppDispatch()
 
-  const addTaskHandler = (titleTasks: string) => {
-    dispatch(createTaskTC({ todolistId: todolist.id, title: titleTasks }))
+    const [createTask,{}] = useCreateTaskMutation()
+
+  const createTaskHandler = (titleTasks: string) => {
+      createTask({todolistId: todolist.id, title: titleTasks} )
   }
+
+
 
   return (
     <div className="container">
       <TodolistTitle todolist={todolist} disabled={todolist.entityStatus === "loading"} />
-      <CreateItemForm addItem={addTaskHandler} disabled={todolist.entityStatus === "loading"} />
+      <CreateItemForm addItem={createTaskHandler} disabled={todolist.entityStatus === "loading"} />
       <Tasks todolist={todolist} disabled={todolist.entityStatus === "loading"} />
       <FilterButtons todolist={todolist} />
     </div>
