04da733328f993aba60a5b312f02e01742acff19
lopatop <pavellopato327@gmail.com>
2025-05-12 20:49:52 +0200

    zod valid extraOptions

--- src/features/todolists/api/tasksApi.ts
+++ src/features/todolists/api/tasksApi.ts
@@ -1,29 +1,40 @@
 import {baseApi} from "@/app/baseApi.ts";
-import {BaseResponse, GetTasksResponse, UpdateTaskModel} from "@/features/todolists/api/tasksApi.types.ts";
+import {
+    GetTasksResponse, getTasksSchema,
+    TaskOperationResponse, taskOperationResponseSchema,
+    UpdateTaskModel
+} from "@/features/todolists/api/tasksApi.types.ts";
+import {DefaultResponse, defaultResponseSchema} from "@/common/types/types.ts";
+
 
 export const tasksApi = baseApi.injectEndpoints({
 
     endpoints: (build) => ({
         getTasks: build.query<GetTasksResponse, string>({
             providesTags: ["Task"],
-            query: (todolistId) => `/todo-lists/${todolistId}/tasks`
+            query: (todolistId) => `/todo-lists/${todolistId}/tasks`,
+            extraOptions: {dataSchema: getTasksSchema},
+
         }),
-        createTask: build.mutation<BaseResponse, { todolistId: string; title: string }>({
+
+        createTask: build.mutation<TaskOperationResponse, { todolistId: string; title: string }>({
             invalidatesTags: ["Task"],
             query: ({todolistId, title}) => ({
                 url: `/todo-lists/${todolistId}/tasks`,
                 method: "POST",
                 body: {title}
-            })
+            }),
+            extraOptions: {dataSchema: taskOperationResponseSchema},
         }),
-        deleteTask: build.mutation<BaseResponse, { todolistId: string; taskId: string }>({
+        deleteTask: build.mutation<DefaultResponse, { todolistId: string; taskId: string }>({
             invalidatesTags: ["Task"],
             query: ({todolistId, taskId}) => ({
                 url: `/todo-lists/${todolistId}/tasks/${taskId}`,
                 method: "DELETE"
-            })
+            }),
+            extraOptions: {dataSchema:defaultResponseSchema },
         }),
-        updateTask: build.mutation<BaseResponse, {
+        updateTask: build.mutation<TaskOperationResponse, {
             todolistId: string;
             taskId: string;
             domainModel: Partial<UpdateTaskModel>
@@ -33,7 +44,8 @@ export const tasksApi = baseApi.injectEndpoints({
                 url: `/todo-lists/${todolistId}/tasks/${taskId}`,
                 method: "PUT",
                 body: domainModel,
-            })
+            }),
+            extraOptions: {dataSchema: taskOperationResponseSchema},
         })
     })
 })
