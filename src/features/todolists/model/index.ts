ab15100f780b6321f303f5f878d75dcda90f850f
lopatop <pavellopato327@gmail.com>
2025-05-12 20:44:24 +0200

    delete novalid fail

--- src/features/todolists/model/index.ts
+++
@@ -1,4 +0,0 @@
-export { tasksSlice } from "./tasks-slice"
-export { selectTasks } from "./tasks-selectors"
-export { selectTodolists } from "./todolist-selectors"
-export { todolistsSlice } from "./todolists-slice"
--- src/features/todolists/ui/Todolists/TodolistItem/FilterButtons/FilterBittons.styles.ts
+++
@@ -1,3 +0,0 @@
-import { SxProps } from "@mui/material"
-
-export const filterButtonContainerStyle: SxProps = { display: "flex", justifyContent: "space-between", gap: "10px" }
