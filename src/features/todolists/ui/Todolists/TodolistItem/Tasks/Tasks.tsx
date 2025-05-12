a134fefb56d689c375bec728475cb180770b2a8c
lopatop <pavellopato327@gmail.com>
2025-05-12 20:49:29 +0200

    useGetTasksQuery

--- src/features/todolists/ui/Todolists/TodolistItem/Tasks/Tasks.tsx
+++ src/features/todolists/ui/Todolists/TodolistItem/Tasks/Tasks.tsx
@@ -1,14 +1,11 @@
 import List from "@mui/material/List"
 import { useAutoAnimate } from "@formkit/auto-animate/react"
-import { useAppSelector } from "@/common/hooks/useAppSelector"
-import { selectTasks } from "@/features/todolists/model/tasks-selectors"
-import { TaskItem } from "@/features/todolists/ui/Todolists/TodolistItem/Tasks/TaskItem/TaskItem"
 import { DomainTask } from "@/features/todolists/api/tasksApi.types"
 import { TaskStatus } from "@/common/enums/enums"
-import { useAppDispatch } from "@/common/hooks"
-import { useEffect } from "react"
-import { fetchTasksTC } from "@/features/todolists/model/tasks-slice"
-import { DomainTodolist } from "@/features/todolists/model/todolists-slice.ts"
+import {useGetTasksQuery} from "@/features/todolists/api/tasksApi.ts";
+import {TasksSkeleton} from "./TasksSkeleton/index.ts";
+import {DomainTodolist} from "@/features/todolists/lib/types";
+import {TaskItem} from "./TaskItem";
 
 type TodolistPropsType = {
   todolist: DomainTodolist
@@ -16,22 +13,30 @@ type TodolistPropsType = {
 }
 export const Tasks = (props: TodolistPropsType) => {
   const { todolist, disabled } = props
-
   const [parent] = useAutoAnimate<HTMLUListElement>()
-  const tasks = useAppSelector(selectTasks)
 
-  const dispatch = useAppDispatch()
-  useEffect(() => {
-    dispatch(fetchTasksTC(todolist.id))
-  }, [])
 
-  let currentTasks: DomainTask[] = tasks[todolist.id] || []
-  if (todolist.filter === "active") currentTasks = currentTasks.filter((t) => t.status === TaskStatus.New)
-  if (todolist.filter === "completed") currentTasks = currentTasks.filter((t) => t.status === TaskStatus.Completed)
+
+const {data, isLoading} = useGetTasksQuery(todolist.id, {
+  skip: !todolist.id,
+})
+
+
+
+
+
+  if(isLoading){
+    return <TasksSkeleton/>
+  }
+
+  let currentTasks = data?.items
+  if (todolist.filter === "active") currentTasks = currentTasks?.filter((t) => t.status === TaskStatus.New)
+  if (todolist.filter === "completed") currentTasks = currentTasks?.filter((t) => t.status === TaskStatus.Completed)
+
 
   return (
     <>
-      {currentTasks.length ? (
+      {currentTasks?.length ? (
         <List ref={parent}>
           {currentTasks.map((t: DomainTask) => (
             <TaskItem key={t.id} todolistId={todolist.id} t={t} disabled={disabled} />
