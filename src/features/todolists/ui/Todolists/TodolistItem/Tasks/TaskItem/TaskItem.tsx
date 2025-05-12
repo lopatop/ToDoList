f270f1dcba4bbc8511a7b293138b313e148c982a
lopatop <pavellopato327@gmail.com>
2025-05-12 20:49:14 +0200

    useDeleteTaskMutation and useUpdateTaskMutation RTK QUERY

--- src/features/todolists/ui/Todolists/TodolistItem/Tasks/TaskItem/TaskItem.tsx
+++ src/features/todolists/ui/Todolists/TodolistItem/Tasks/TaskItem/TaskItem.tsx
@@ -1,81 +1,83 @@
 import ListItem from "@mui/material/ListItem"
 import Box from "@mui/material/Box"
 import Checkbox from "@mui/material/Checkbox"
-import { deleteTasksTC, updateTaskTC } from "@/features/todolists/model/tasks-slice"
-import { EditableSpan } from "@/common/components/EditableSpan/EditableSpan"
 import IconButton from "@mui/material/IconButton"
 import Clear from "@mui/icons-material/Clear"
-import { useAppDispatch } from "@/common/hooks/useAppDispatch"
-import { listItemStyle } from "@/features/todolists/ui/Todolists/TodolistItem/Tasks/TaskItem/TaskItem.styles"
-import { TaskStatus } from "@/common/enums/enums"
-import { DomainTask } from "@/features/todolists/api/tasksApi.types"
-import { useAppSelector } from "@/common/hooks"
-import { selectStatus } from "@/app/app-slice.ts"
+import {TaskStatus} from "@/common/enums/enums"
+import {useAppSelector} from "@/common/hooks"
+import {selectStatus} from "@/app/app-slice.ts"
+import {useDeleteTaskMutation, useUpdateTaskMutation} from "@/features/todolists/api/tasksApi.ts";
+import {ChangeEvent} from "react";
+import {DomainTask, UpdateTaskModel} from "@/features/todolists/api";
+import {listItemStyle} from "./TaskItem.styles";
+import {EditableSpan} from "@/common/components";
 
 type TaskProps = {
-  t: DomainTask
-  todolistId: string
-  disabled: boolean
+    t: DomainTask
+    todolistId: string
+    disabled: boolean
 }
 
-export const TaskItem = ({ disabled, t, todolistId }: TaskProps) => {
-  const dispatch = useAppDispatch()
-  const status = useAppSelector(selectStatus)
+export const TaskItem = ({disabled, t, todolistId}: TaskProps) => {
+    const [deleteTask, {isLoading}] = useDeleteTaskMutation()
+    const [updateTask] = useUpdateTaskMutation()
+    const status = useAppSelector(selectStatus)
 
-  return (
-    <ListItem sx={listItemStyle}>
-      <Box>
-        <Checkbox
-          size={"small"}
-          checked={t.status === TaskStatus.Completed}
-          disabled={disabled}
-          onChange={(e) =>
-            dispatch(
-              updateTaskTC({
-                todolistId,
-                taskId: t.id,
-                domainModel: {
-                  title: t.title,
-                  description: t.description,
-                  status: e.currentTarget.checked ? TaskStatus.Completed : TaskStatus.New,
-                  priority: t.priority,
-                  startDate: t.startDate,
-                  deadline: t.deadline,
-                },
-              }),
-            )
-          }
-        />
 
-        <EditableSpan
-          disabled={disabled}
-          isDone={t.status === TaskStatus.Completed}
-          title={t.title}
-          changeTitleItem={(newTitleTask) =>
-            dispatch(
-              updateTaskTC({
-                todolistId,
-                taskId: t.id,
-                domainModel: {
-                  title: newTitleTask,
-                  description: t.description,
-                  status: t.status,
-                  priority: t.priority,
-                  startDate: t.startDate,
-                  deadline: t.deadline,
-                },
-              }),
-            )
-          }
-        />
-      </Box>
-      <IconButton
-        aria-label="delete"
-        onClick={() => dispatch(deleteTasksTC({ todolistId, taskId: t.id }))}
-        disabled={disabled || status === "loading"}
-      >
-        <Clear />
-      </IconButton>
-    </ListItem>
-  )
+
+    const updateTaskModel = (updatedFields: Partial<UpdateTaskModel>) => {
+        const model: UpdateTaskModel = {
+            description: t.description,
+            title: t.title,
+            priority: t.priority,
+            startDate: t.startDate,
+            deadline: t.deadline,
+            status: t.status,
+
+            ...updatedFields
+        }
+        updateTask({todolistId, taskId: t.id, domainModel: model});
+    };
+
+
+    const deleteTaskHandler = () => {
+        deleteTask({todolistId, taskId: t.id})
+    }
+    const updateStatusTask = (e: ChangeEvent<HTMLInputElement>) => {
+        let status = e.currentTarget.checked ? TaskStatus.Completed : TaskStatus.New
+        updateTaskModel({status})
+    }
+    const updateTitleTask = (newTitleTask: string) => {
+        updateTaskModel({title: newTitleTask})
+    }
+
+
+    return (
+        <ListItem sx={listItemStyle}>
+            <Box>
+                <Checkbox
+                    size={"small"}
+                    checked={t.status === TaskStatus.Completed}
+                    disabled={disabled || isLoading}
+                    onChange={updateStatusTask}
+
+                />
+
+                <EditableSpan
+                    isLoading ={isLoading}
+                    disabled={disabled}
+                    isDone={t.status === TaskStatus.Completed}
+                    title={t.title}
+                    changeTitleItem={updateTitleTask}
+                />
+            </Box>
+            <IconButton
+                aria-label="delete"
+                onClick={deleteTaskHandler}
+                disabled={disabled || status === "loading"}
+            >
+                <Clear/>
+            </IconButton>
+        </ListItem>
+    )
 }
