69530d0c2209491a99fcc4aaa7b90c27ad1b6eeb
lopatop <pavellopato327@gmail.com>
2025-05-12 20:53:24 +0200

    add isLoading Skeleton

--- src/features/todolists/ui/Todolists/Todolists.tsx
+++ src/features/todolists/ui/Todolists/Todolists.tsx
@@ -1,17 +1,30 @@
 import Grid from "@mui/material/Grid"
 import Paper from "@mui/material/Paper"
-import {TodolistItem} from "@/features/todolists/ui/Todolists/TodolistItem/TodolistItem"
-import {paperStyle} from "@/features/todolists/ui/Todolists/Todolists.styles"
+import {TodolistItem} from "./TodolistItem/index"
+import {paperStyle} from "./Todolists.styles"
 import {useGetTodolistsQuery} from "@/features/todolists/api/todolistsApi.ts";
+import {containerSx} from "@/common/styles";
+import Box from "@mui/material/Box";
+import {TodolistSkeleton} from "./TodolistSkeleton/index";
 
 
 export const Todolists = () => {
 
-    const {data: todolist} = useGetTodolistsQuery()
+    const {data: todolist, isLoading} = useGetTodolistsQuery()
+
+
+    if (isLoading) {
+      return (
+          <Box sx ={containerSx} style={{gap: "20px", flexWrap: "wrap"}}>
+              {Array(3).fill(null).map((_,id)=>( <TodolistSkeleton key={id}/>))}
+          </Box>
+      )
+    }
 
     return (
         <Grid container>
             {todolist?.map((el) => {
+                if(!el.id) return null
                 return (
                     <Grid item sx={{p: "30px"}} key={el.id}>
                         <Paper elevation={5} sx={paperStyle}>
