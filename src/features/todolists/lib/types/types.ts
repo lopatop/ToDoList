716f3d746ed3a67d27d741094a8c6b3ba130923b
lopatop <pavellopato327@gmail.com>
2025-05-19 23:59:26 +0200

    remove entityStatus

--- src/features/todolists/lib/types/types.ts
+++ src/features/todolists/lib/types/types.ts
@@ -1,10 +1,8 @@
 
 import type { Todolist } from "@/features/todolists/api/todolistsApi.types"
-import {RequestStatus} from "@/common/types/types.ts";
 
 export type DomainTodolist = Todolist & {
     filter: FilterValues
-    entityStatus: RequestStatus
 }
 
 export type FilterValues = "all" | "active" | "completed"
