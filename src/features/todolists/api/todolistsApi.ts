22ed2254811fa18aa2cf759accbf8f4fbbfb278f
lopatop <pavellopato327@gmail.com>
2025-05-20 00:03:48 +0200

    add extraOptions Optimistic update

--- src/features/todolists/api/todolistsApi.ts
+++ src/features/todolists/api/todolistsApi.ts
@@ -1,7 +1,11 @@
-import {CreateTodolistResponse, Todolist} from "@/features/todolists/api/todolistsApi.types.ts";
+import {
+    CreateTodolistResponse,
+    createTodolistResponseSchema,
+    Todolist
+} from "@/features/todolists/api/todolistsApi.types.ts";
 import {baseApi} from "@/app/baseApi.ts";
 import {DomainTodolist} from "@/features/todolists/lib/types";
-import {DefaultResponse} from "@/common/types/types.ts";
+import {DefaultResponse, defaultResponseSchema} from "@/common/types";
 
 export const todolistsApi = baseApi.injectEndpoints({
     endpoints: build => ({
@@ -9,23 +13,41 @@ export const todolistsApi = baseApi.injectEndpoints({
             providesTags: ["Todolist"],
             query: () => "/todo-lists",
             transformResponse: (res: Todolist[]) => {
-                return res.map(tl => ({...tl, filter: 'all', entityStatus: 'idle'}))
+                return res.map(tl => ({...tl, filter: 'all'}))
             }
         }),
+
         changeTodolistTitle: build.mutation<DefaultResponse, { id: string; title: string }>({
             invalidatesTags: ["Todolist"],
             query: ({id, title}) => ({
                 url: `/todo-lists/${id}`,
                 method: "PUT",
                 body: {title}
-            })
+            }),
+            extraOptions: {dataSchema: defaultResponseSchema},
         }),
         deleteTodolist: build.mutation<DefaultResponse, string>({
             invalidatesTags: ["Todolist"],
             query: (id) => ({
                 url: `/todo-lists/${id}`,
                 method: "DELETE"
-            })
+            }),
+            extraOptions: {dataSchema: defaultResponseSchema},
+            async onQueryStarted(id: string, {dispatch, queryFulfilled}) {
+                const patchResult = dispatch(
+                    todolistsApi.util.updateQueryData('getTodolists', undefined, state => {
+                        const index = state.findIndex(todolist => todolist.id === id)
+                        if (index !== -1) {
+                            state.splice(index, 1)
+                        }
+                    })
+                )
+                try {
+                    await queryFulfilled
+                } catch {
+                    patchResult.undo()
+                }
+            },
         }),
         createTodolist: build.mutation<CreateTodolistResponse, string>({
             invalidatesTags: ["Todolist"],
@@ -33,8 +55,10 @@ export const todolistsApi = baseApi.injectEndpoints({
                 url: `/todo-lists`,
                 method: "POST",
                 body: {title}
-            })
+            }),
+            extraOptions: {dataSchema: createTodolistResponseSchema},
         })
+
     })
 })
 
--- src/features/todolists/ui/Todolists/Todolists.styles.ts
+++ src/features/todolists/ui/Todolists/Todolists.styles.ts
@@ -1,2 +1,2 @@
 import { SxProps } from "@mui/material"
-export const paperStyle: SxProps = { p: "30px", borderRadius: "15px" }
+export const paperStyle: SxProps = { p: "30px", borderRadius: "15px", minHeight:"450px" }
